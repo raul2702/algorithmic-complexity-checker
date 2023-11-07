1e7156c8f77c2bf3ab3cfe6e3f70dc6d1b965baa
Raul Chavez <raul_alex_10_@hotmail.com>
2023-11-06 21:34:02 -0600

    argparse basic structure added

--- src/ComplexityChecker.py
+++ src/ComplexityChecker.py
@@ -1,1 +1,24 @@
+import argparse
 
+
+def main():
+    parser = argparse.ArgumentParser(description='This program will check the algorithmic complexity of a python code')
+
+    parser.add_argument('--file_check', '-fic', type=str, help='This parameter is used to check the algorithmic complexity of the provided .py file')
+    file_check()
+
+    parser.add_argument('--function_check', '-fuc', type=str, help='This parameter is used to check the algorithmic complexity of an specific funcion of the provided .py file')
+    function_check()
+
+    args = parser.parse_args()
+    #print(f'hola: {args.file_check}')
+
+
+def file_check():
+    pass
+
+def function_check():
+    pass
+
+if __name__ == "__main__":
+    main()

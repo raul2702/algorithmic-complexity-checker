fff1c5d2a69e0f6d8ba2a0dd88ed385a36ac640f
Raul Chavez <raul_alex_10_@hotmail.com>
2023-11-12 13:29:55 -0600

    added argparse with functions file_check & function_check

--- src/ComplexityChecker.py
+++ src/ComplexityChecker.py
@@ -5,19 +5,23 @@ def main():
     parser = argparse.ArgumentParser(description='This program will check the algorithmic complexity of a python code')
 
     parser.add_argument('--file_check', '-fic', type=str, help='This parameter is used to check the algorithmic complexity of the provided .py file')
-    file_check()
-
     parser.add_argument('--function_check', '-fuc', type=str, help='This parameter is used to check the algorithmic complexity of an specific funcion of the provided .py file')
-    function_check()
 
     args = parser.parse_args()
-    #print(f'hola: {args.file_check}')
 
+    if args.file_check:
+        file_check(args.file_check)
+    elif args.function_check:
+        function_check(args.function_check)
+    else:
+        print('Choose one of the following parse arguments: \n\t--file-check, -fic \n\t--function-check, -fuc')
 
-def file_check():
+def file_check(arg):
+    print(arg)
     pass
 
-def function_check():
+def function_check(arg):
+    print(arg)
     pass
 
 if __name__ == "__main__":

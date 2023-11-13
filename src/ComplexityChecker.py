dab2891e523dd377e5a2f8812eedd277287499bd
Bryan Aguirre <bryaguir@ibm.com>
2023-11-13 09:10:35 -0600

    adding On logic

--- src/ComplexityChecker.py
+++ src/ComplexityChecker.py
@@ -1,1 +1,41 @@
+import re
 
+class ComplexityChecker:
+    def __init__(self, file, func):
+        self.file = file
+        self.func = func
+
+    def On_algorithmic_complexity(self):
+        pattern = re.compile(r'\b(?:for|while)\b')
+        function_content = self.get_function_text()
+
+        indentation = 0
+        complexity = 0
+        biggest_identation = 0
+        for element in function_content:
+            if pattern.search(element):
+                indentation = len(element) - len(element.lstrip())
+                if indentation > biggest_identation:
+                    biggest_identation = indentation
+                    complexity = complexity + 1
+
+        return complexity
+    def get_function_text(self):
+        with open(self.file, 'r') as code:
+            in_func = False
+            func_lines = []
+
+            for line in code:
+                if re.search(fr"def {self.func}\(", line):
+                    in_func = True
+                    func_lines.append(line)
+                elif in_func:
+                    if re.search(r"def", line): 
+                        break 
+                    func_lines.append(line)
+        return func_lines
+
+func = "take_node_snaps"
+file= "/Users/bryaguir/Documents/Info_personal/Master/Diseño de algoritmos/CC_project/algorithmic-complexity-checker/test_scripts/test_script_On_complex.py"
+test = ComplexityChecker(file, func)
+print(test.On_algorithmic_complexity())
---
+++ test_scripts/test_script_On_complex.py
@@ -0,0 +1,53 @@
+import threading
+import time
+import os
+import sys
+
+clusters = ["mcr-cay-cluster-15"]
+hosts=["mcr-host-253"]
+user = "superuser"
+port = "22"
+
+format_string = "==============================================================================================================================================================================="
+livedump = "no"
+concurrentnodedumps = "no"
+svc_snap = "no"
+host_snap="no"
+
+def take_concurrent_node_dump(cluster_list):
+    for cluster in cluster_list:
+        print("Taking concurrent node dumps on cluster: " + cluster)
+        result, output, stderr = test_utils.runSSHCommand(user, cluster, "svctask concurrentnodedump", port)
+        if(result):
+            print(output[0].decode('utf-8'))
+            time.sleep(300)
+        else: 
+            print(stderr[0].decode('utf-8'))
+
+def take_livedumps(cluster_list):
+    for cluster in cluster_list:
+        print("taking livedump on on cluster: " + cluster)
+        node_list = build_node_list(cluster)
+       # print(str(len(node_list)))
+        test_utils.runSSHCommand(user, cluster, "for i in {1.." + str(len(node_list)) + "}; do svctask preplivedump node$i;done", port)
+       
+        print("Prepared livedump on all nodes in cluster: "+ cluster + ", pausing for 10 seconds")
+        time.sleep(10)
+
+        print("Triggering livedumps on all nodesin cluster: " + cluster + ", this can take 5 minutes to complete.")
+        test_utils.runSSHCommand(user, cluster, "for i in {1.." + str(len(node_list)) + "}; do svctask triggerlivedump node$i;done", port)
+        time.sleep(300)
+        print("livedumps collection completed on cluster: " + str(cluster))
+
+def take_node_snaps(cluster_list):
+    for cluster in cluster_list:
+        print("Taking node snaps on cluster: " + cluster + "..... \n Please be patient this can take some time... ")
+        result, snap_output, stderr = test_utils.runSSHCommand(user, cluster, "svc_snap -a", port)
+        if(result):
+            time.sleep(240)
+            print("Please continue to wait, snaps are still being taken.")
+            time.sleep(240)
+            for line in snap_output:
+                print(line.decode('utf-8'))
+        else:
+            print(stderr[0].decode('utf-8'))

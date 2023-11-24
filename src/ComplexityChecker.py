75f60868ff3826126de37b7bbf64bb9d7393381c
BryanAguirre1 <70287991+BryanAguirre1@users.noreply.github.com>
2023-11-24 12:00:10 -0600

    Update ComplexityChecker.py

--- src/ComplexityChecker.py
+++ src/ComplexityChecker.py
@@ -4,21 +4,15 @@ import argparse
 def main():
     parser = argparse.ArgumentParser(description='This program will check the algorithmic complexity of a python code')
 
-    parser.add_argument('--file_check', '-fic', type=str, help='This parameter is used to check the algorithmic complexity of the provided .py file')
-    parser.add_argument('--function_check', '-fuc', type=str, help='This parameter is used to check the algorithmic complexity of an specific funcion of the provided .py file')
+    parser.add_argument('--file', '-f', type=str, help='This parameter is used to identify .py file of the function')
+    parser.add_argument('--function-to-check', '-func', type=str, help='This parameter is used to check the algorithmic complexity of an specific funcion of the provided .py file')
 
     args = parser.parse_args()
 
-    if args.file_check:
-        file_check(args.file_check)
-    elif args.function_check:
+    if args.function_check and args.file:
         function_check(args.function_check)
     else:
-        print('Choose one of the following parse arguments: \n\t--file-check, -fic \n\t--function-check, -fuc')
-
-def file_check(arg):
-    print(arg)
-    pass
+        print('Type the following parse arguments: \n\t--file, -f \n\t--function-to-check, -func')
 
 def function_check(arg):
     print(arg)
